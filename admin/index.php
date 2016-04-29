18f1ea6eb92d238ea14ace8d9ca7b4e907180306
Thiago <thisimaso@gmail.com>
2016-04-29 16:01:09 -0300

    modificações na index e começo do metodo para login do administrador

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,15 @@
+<?php
+    if(is_array($_POST) && count($_POST) > 0){
+        if($_POST["action"] == "login"){
+
+        }
+    }//fecha primeiro if
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>alooo</title>
+	<title>Adm</title>
 	<meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -12,13 +20,14 @@
 
 <div class="container">
 
-    <form class="form-signin">
+    <form class="form-signin" method="post" action="">
         <h1 class="form-signin-heading text-muted">Sign In</h1>
-        <input type="text" class="form-control" placeholder="Email address" required="" autofocus="">
+        <input type="text" class="form-control" placeholder="User or email address" required="" autofocus="">
         <input type="password" class="form-control" placeholder="Password" required="">
         <button class="btn btn-lg btn-primary btn-block" type="submit">
             Sign In
         </button>
+        <input type="hidden" name="action" value="login">
     </form>
 
 </div>
