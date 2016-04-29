c678f608a93efad35ce47991c0ebd3df966cb253
Thiago <thisimaso@gmail.com>
2016-04-29 11:36:41 -0300

    criando e organizando pastas, inicio do db e da conexão com o banco

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * User: lhaos
+ * Date: 29/04/2016
+ */
+    return array(
+        'default' => array(
+            'dsn' => 'mysql:host=localhost;dbname=blog',
+            'user' => 'root',
+            'password' => 'root'
+        )
+    );
---
+++ src/conection/Db.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * User: lhaos
+ * Date: 29/04/2016
+ */
+include_once '\config\db.php';
+
+class Db extends PDO{
+
+    private static $instanc = null;
+
+
+
+}//fecha class
